8aeb3242fd6e5ce613957843238dbab490950595
Soukayna-Boucetta <soukaboustta1999@gmail.com>
2023-08-16 00:47:32 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import ToDo from './components/ToDo';
+
+
+
+function App() {
+  
+  return(
+    <div className="container">
+      <ToDo />
+  
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/ToDo.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './ToDo.css';
+
+const ToDo = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="todo-container">
+      <h2>Todo List</h2>
+      <div className="todo-input">
+        <input
+          type="text"
+          placeholder="Enter a new todo"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo}
+            <button onClick={() => removeTodo(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ToDo;
